5a1d3b51a48fa36791c44daeeb094924a603c128
LiuFei <wendaofeifei@126.com>
2021-06-25 14:11:46 +0800

    在分支中修改内容然后提交

---
+++ src/test/java/common/GitTest.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * @Description
+ * @Author feiliu6
+ * @Date 14:09 2021/6/25
+ * @Version 1.0
+ */
+public class GitTest {
+    public void test(){
+        System.out.println("edit in branch ..");
+    }
+}
